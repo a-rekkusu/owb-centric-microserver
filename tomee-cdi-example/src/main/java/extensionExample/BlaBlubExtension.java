669628f27e330740fbf319a4a837a91b7cdcf40a
Alexander Fischer <fischer.a@mailbox.org>
2020-06-02 17:15:48 +0200

    added extension methods and fixed structure

--- tomee-cdi-example/src/main/java/extensionExample/BlaBlubExtension.java
+++ tomee-cdi-example/src/main/java/extensionExample/BlaBlubExtension.java
@@ -2,13 +2,43 @@ package extensionExample;
 
 import javax.enterprise.event.Observes;
 import javax.enterprise.inject.spi.*;
+import java.util.ArrayList;
 
 public class BlaBlubExtension implements Extension
 {
+    private static ArrayList<String> list = new ArrayList<String>();
 
     <T> void processAnnotatedType(@Observes @WithAnnotations(BlaBlubQualifier.class) ProcessAnnotatedType<T> patEvent)
     {
-        System.out.println("scanning type: " + patEvent.getAnnotatedType().getJavaClass().getName());
+        System.out.println("Process Annotated Type: " + patEvent.getAnnotatedType().getJavaClass().getName());
+        list.add(patEvent.getAnnotatedType().getJavaClass().getName());
+    }
+
+    void afterTypeDiscovery(@Observes AfterTypeDiscovery atd)
+    {
+        System.out.println("----AFTER TYPE DISCOVERY----");
+        for (String s : list)
+        {
+            System.out.println("After Type Discovery: " + s);
+        }
+    }
+
+    void afterBeanDiscovery(@Observes AfterBeanDiscovery abd)
+    {
+        System.out.println("----AFTER BEAN DISCOVERY----");
+        for (String s : list)
+        {
+            System.out.println("After Bean Discovery: " + s);
+        }
+    }
+
+    void afterDeploymentValidation(@Observes AfterDeploymentValidation adv)
+    {
+        System.out.println("----AFTER DEPLOYMENT VALIDATION----");
+        for (String s : list)
+        {
+            System.out.println("After Deployment Validation: " + s);
+        }
     }
 
 }
